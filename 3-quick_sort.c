31b07a6de64591bdcd7a843a504d372cfd9042ec
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2025-12-11 11:11:58 +0000

    Fixed betty docs and formatting

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,10 @@
 #include "sort.h"
 
+/**
+ * swap - Swap to integers variables
+ * @a: The first integer
+ * @b: The second integer
+*/
 void swap(int *a, int *b)
 {
 	int x = *a;
@@ -7,17 +12,25 @@ void swap(int *a, int *b)
 	*b = x;
 }
 
-size_t partition(int* array, int lo, int hi, size_t size)
+/**
+ * partition - Lomuto partition algo
+ * @array: The int array to partition
+ * @lo: The lowest index
+ * @hi: The highest index
+ * @size: The size of the array
+ * Return: Final index of pivot
+*/
+size_t partition(int *array, int lo, int hi, size_t size)
 {
 	int pivot = array[hi];
 	int i = lo;
 	int j;
 
-	for(j = lo; j < hi; ++j)
+	for (j = lo; j < hi; ++j)
 	{
-		if(array[j] <= pivot)
+		if (array[j] <= pivot)
 		{
-			if(i != j)
+			if (i != j)
 			{
 				swap(&array[i], &array[j]);
 				print_array(array, size);
@@ -25,19 +38,26 @@ size_t partition(int* array, int lo, int hi, size_t size)
 			++i;
 		}
 	}
-	if(i != hi)
+	if (i != hi)
 	{
 		swap(&array[i], &array[hi]);
 		print_array(array, size);
 	}
-	return i;
+	return (i);
 }
 
-void quicksort(int* array, int lo, int hi, size_t size)
+/**
+ * quicksort - Quicksort algo
+ * @array: The array to sort
+ * @lo: The lowest index
+ * @hi: The highest index
+ * @size: Size of the array
+*/
+void quicksort(int *array, int lo, int hi, size_t size)
 {
 	size_t p;
 
-	if(lo >= hi || lo < 0)
+	if (lo >= hi || lo < 0)
 		return;
 
 	p = partition(array, lo, hi, size);
@@ -46,7 +66,11 @@ void quicksort(int* array, int lo, int hi, size_t size)
 	quicksort(array, p + 1, hi, size);
 }
 
-
+/**
+ * quick_sort - Quicksort algo wrapper
+ * @array: The array to sort
+ * @size: Size of the array
+*/
 void quick_sort(int *array, size_t size)
 {
 	quicksort(array, 0, size - 1, size);
